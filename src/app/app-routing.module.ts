ca3512f36ab78688cd58f318ba71659f3d5ee7dc
ramyas13 <ramyashree1301@gmail.com>
2022-10-05 14:46:37 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FertilizerComponent } from './fertilizer/fertilizer.component';
-
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
   {path:'ferti',component:FertilizerComponent},
+  {path:'contact',component:ContactComponent},
 ];
 
 @NgModule({
