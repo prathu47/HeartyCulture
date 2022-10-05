fd4b9ea42b5eebc2292a35867715f17317dc2c62
prathusha <prathusha>
2022-10-05 19:56:26 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NavnextComponent } from './navnext/navnext.component';
-<<<<<<< HEAD
 import { FruitTreesComponent } from './fruit-trees/fruit-trees.component';
-=======
 import { FertilizerComponent } from './fertilizer/fertilizer.component';
 import { ContactComponent } from './contact/contact.component';
->>>>>>> 3dd607a5c6d2da8c21995eeb998b73ef58a55a36
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     NavnextComponent,
-<<<<<<< HEAD
-    FruitTreesComponent
-=======
+    FruitTreesComponent,
     FertilizerComponent,
     ContactComponent
->>>>>>> 3dd607a5c6d2da8c21995eeb998b73ef58a55a36
   ],
   imports: [
     BrowserModule,
