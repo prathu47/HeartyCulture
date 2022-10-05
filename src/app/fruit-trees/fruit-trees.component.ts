406ac76bf8f5d9fcfe9b013b5bb4d96affb6155b
prathusha <prathusha>
2022-10-05 22:18:19 +0530

    update

--- src/app/fruit-trees/fruit-trees.component.ts
+++ src/app/fruit-trees/fruit-trees.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import * as prod from "src/app/fruit-trees/fruits.json";
 @Component({
   selector: 'app-fruit-trees',
   templateUrl: './fruit-trees.component.html',
@@ -9,7 +9,13 @@ export class FruitTreesComponent implements OnInit {
 
   constructor() { }
 
+  @Input()product:any
+
   ngOnInit(): void {
   }
+  productDetails=false;
+  //Event to perform the toggle effect
+  
+  productDetail:any=(prod as any).default;
 
 }
