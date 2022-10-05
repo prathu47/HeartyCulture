d3036eec65868cae9389140f8be1cfa21b7ae8e4
ramyas13 <ramyashree1301@gmail.com>
2022-10-05 11:25:16 +0530

    commit

---
+++ src/app/navnext/navnext.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navnext',
+  templateUrl: './navnext.component.html',
+  styleUrls: ['./navnext.component.css']
+})
+export class NavnextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
