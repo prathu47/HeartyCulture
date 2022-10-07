90c3fb793f499c28ff3dba39739f7e14627d7cfc
Sampada7059 <sampadapolepalli7@gmail.com>
2022-10-07 14:58:22 +0530

    commit

---
+++ src/app/flower-seeds/flower-seeds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flower-seeds',
+  templateUrl: './flower-seeds.component.html',
+  styleUrls: ['./flower-seeds.component.css']
+})
+export class FlowerSeedsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
