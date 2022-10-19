07aad723833a4b119eacea9984e0d2361b05efca
prathusha <prathusha>
2022-10-20 00:15:37 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   postsapi:"http://localhost:3000/posts",
   cartapi:"http://localhost:3000/cart",
-  userapi:"https://localhost:7122/api/Registrations",
+  userapi:"http://localhost:3000/users",
   contactapi:"http://localhost:3000/contact",
   subscribeapi:"http://localhost:3000/subscribe"
 
